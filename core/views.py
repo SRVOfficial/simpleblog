c84fabefa75c0618b6395e731622fe2ffc753fd2
SRVOfficial <sauryatapish@gmail.com>
2024-08-28 17:43:55 +0530

    first commit

---
+++ core/views.py
@@ -0,0 +1,22 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+# import the Post from our blog app model
+from blog.models import Post
+
+# Create your views here.
+def frontpage(request):
+    # posts = Post.objects.all()
+    posts = Post.objects.filter(status = Post.ACTIVE)
+    return render(request, 'core/frontpage.html', {'posts' : posts})
+
+def about(request):
+    return render(request, 'core/about.html')
+
+def robots_txt(request):
+    text = [
+        "User-Agent: *",
+        "Disallow: /admin/",
+    ]
+
+    return HttpResponse("\n".join(text), content_type = "text/plain")
